58a3d7569a12213622d85138541629b720be4425
Priyanshu Singh <priyanshusinghdev2003@gmail.com>
2025-12-13 20:58:53 +0530

    Add immer for state management and implement authentication store

---
+++ src/store/Auth.js
@@ -0,0 +1,80 @@
+import { immer, Immer } from "zustand/middleware/immer";
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import { ID } from "appwrite";
+import { account } from "@/models/client/config";
+
+export const useAuthStore = create()(
+  persist(
+    immer((set) => ({
+      session: null,
+      jwt: null,
+      user: null,
+      hydrated: false,
+
+      setHydrated() {
+        set({ hydrated: true });
+      },
+
+      async verifySession() {
+        try {
+          const session = await account.getSession("current");
+          set({ session });
+        } catch (error) {
+          console.log(error);
+        }
+      },
+
+      async login(email, password) {
+        try {
+          const session = await account.createEmailPasswordSession(
+            email,
+            password,
+          );
+          const [user, { jwt }] = await Promise.all([
+            account.get(),
+            account.createJWT(),
+          ]);
+          if (!user.prefs?.reputation)
+            await account.updatePrefs({
+              reputation: 0,
+            });
+          set({ session, jwt, user });
+          return { success: true };
+        } catch (error) {
+          console.log(error);
+          return { success: false, error: error };
+        }
+      },
+
+      async createAccount(name, email, password) {
+        try {
+          const user = await account.create(ID.unique(), name, email, password);
+          return { success: true };
+        } catch (error) {
+          console.log(error);
+          return { success: false, error: error };
+        }
+      },
+
+      async logout() {
+        try {
+          await account.deleteSessions();
+          set({ session: null, jwt: null, user: null });
+          return { success: true };
+        } catch (error) {
+          console.log(error);
+          return { success: false, error: error };
+        }
+      },
+    })),
+    {
+      name: "auth",
+      onRehydrateStorage() {
+        return (state, error) => {
+          if (!error) state?.setHydrated();
+        };
+      },
+    },
+  ),
+);
